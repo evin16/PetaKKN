19be770d70a99ecf958b6995a2ccffec5d4ab445
evin16 <eriksonvrianugrah@gmail.com>
2022-07-14 03:06:11 +0700

    First Commit

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Home</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="shortcut icon" type="image/x-icon" href="docs/images/favicon.ico" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+  <div class="jumbotron">
+    <div class="container">
+      <div class="text-center text-white my-5 p-3 rounded-pill bg-dark">
+        <h1>Pemetaan Titik Lokasi KKN UNS 2022</h1>
+      </div>
+      <div class="row my-2">
+        <div class="col px-2">
+          <a href="./Pages/Surakarta.php" class='btn btn-primary w-100'>SURAKARTA</a>
+        </div>
+        <div class="col px-2">
+          <a href="./Pages/Sukoharjo.php" class='btn btn-primary w-100'>SUKOHARJO </a>
+        </div>
+        <div class="col px-2">
+          <a href="./Pages/Boyolali.php" class='btn btn-primary w-100'>BOYOLALI</a>
+        </div>
+        <div class="col px-2">
+          <a href="./Pages/Karanganyar.php" class='btn btn-primary w-100'>KARANGANYAR </a>
+        </div>
+      </div>
+      <div class="row my-2">
+        <div class="col px-2">
+          <a href="./Pages/Sragen.php" class='btn btn-primary w-100'>SRAGEN </a>
+        </div>
+        <div class="col px-2">
+          <a href="./index.php" class='btn btn-primary w-100'>WONOGIRI </a>
+        </div>
+        <div class="col px-2">
+          <a href="./index.php" class='btn btn-primary w-100'>KLATEN </a>
+        </div>
+        <div class="col px-2">
+          <a href="./index.php" class='btn btn-primary w-100'>MAGETAN </a>
+        </div>
+      </div>
+      <div class="row my-2">
+        <div class="col px-2">
+          <a href="./index.php" class='btn btn-primary w-100'>NGAWI </a>
+        </div>
+        <div class="col px-2">
+          <a href="./index.php" class='btn btn-primary w-100'>MAGELANG </a>
+        </div>
+        <div class="col px-2">
+          <a href="./index.php" class='btn btn-primary w-100'>KEBUMEN </a>
+        </div>
+        <div class="col px-2">
+          <a href="./index.php" class='btn btn-primary w-100'>PACITAN </a>
+        </div>
+      </div>
+      <div class="row my-2">
+        <div class="col px-2">
+          <a href="./index.php" class='btn btn-primary w-100'>GROBOGAN </a>
+        </div>
+        <div class="col px-2">
+          <a href="./index.php" class='btn btn-primary w-100'>BREBES </a>
+        </div>
+        <div class="col px-2">
+          <a href="./index.php" class='btn btn-primary w-100'>CILACAP </a>
+        </div>
+        <div class="col px-2">
+          <a href="./index.php" class='btn btn-primary w-100'>PANGANDARAN </a>
+        </div>
+      </div>
+
+
+
+    </div>
+
+  </div>
+  </div>
+</body>
+
+</html>
