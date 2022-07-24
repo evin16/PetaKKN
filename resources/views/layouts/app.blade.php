3743371cb964112fc26c19dbb7a303d7d651f3f4
argaputra12 <argaputra@student.uns.ac.id>
2022-07-25 03:26:05 +0700

    update header and nav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
 
             <!-- Page Heading -->
             <header >
-                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-[750px] font-inter">
+                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-[795px] font-inter">
                     {{ $header }}
                 </div>
             </header>
