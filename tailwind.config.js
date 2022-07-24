1870d513cac75f145e214dac235e6fe3e3f990b4
argaputra12 <argaputra@student.uns.ac.id>
2022-07-25 03:18:50 +0700

    laravel migration, update homepage

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+
+        extend: {
+            colors : {
+                primary : {
+                    textdark : '#100F0F',
+                    textlight : '#F1F1F1',
+                    blue : '#006EA4',
+                    red : '#F66951'
+                }
+            },
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                inter: ['Inter', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [require('@tailwindcss/forms')],
+};
